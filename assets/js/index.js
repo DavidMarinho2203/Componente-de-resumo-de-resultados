94483bc3a1e471edb0f9b45aa090945b93752cb4
David Beckham <davidbeckhampaulo32@gmail.com>
2024-05-14 11:32:48 -0300

    Incluindo todos arquivos

---
+++ assets/js/index.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(objeto => {
+            // Obtém a div de conteúdo
+            const resultado = document.querySelector('h1')
+
+            // Preenche o conteúdo com os dados do JSON
+            const somaScore = (objeto.reduce((acc, element) => acc + element.score, 0) / objeto.length).toFixed(0)
+            resultado.textContent = somaScore
+
+            const ul = document.getElementById('container__categorias')
+            objeto.forEach(element => {
+                // Criando a lista
+                const li = this.createElement('li')
+                li.id = 'categoria__' + element.category
+                li.classList = 'categoria'
+
+                switch (element.category) {
+                    case 'Reaction':
+                        li.style.backgroundColor = 'var(--corC-VermelhoClaro)'
+                        break;
+                    case 'Memory':
+                        li.style.backgroundColor = 'var(--corC-AmareloAlaranja)'
+                        break;
+                    case 'Verbal':
+                        li.style.backgroundColor = 'var(--corC-VerdeAzulado)'
+                        break;
+                    default:
+                        li.style.backgroundColor = 'var(--corC-AzulCobalto)'
+                        break;
+                }
+                // li.style.backgroundColor = 'var(--corC-VermelhoClaro)'
+
+                // Primeira div
+                const div1 = this.createElement('div')
+                div1.classList = 'categoria__Icone_Nome'
+                const img = this.createElement('img')
+                img.src = `${element.icon}`
+                const p1 = this.createElement('p')
+                p1.classList = 'categoria__nome'
+                p1.textContent = element.category
+                div1.append(img, p1)
+                // Segunda div
+                const div2 = this.createElement('div')
+                div2.classList = 'container__valores'
+                const p2 = this.createElement('p')
+                p2.classList = 'categoria__numero'
+                p2.textContent = `/ 100`
+                const span2 = this.createElement('span')
+                span2.textContent = element.score
+                div2.append(span2,p2)
+                // Incluindo no li e depois na ul
+                li.append(div1, div2)
+                ul.appendChild(li)
+
+
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+        })
+        .catch(error => {
+            console.error('Ocorreu um erro ao carregar o JSON:', error);
+        });
+})
